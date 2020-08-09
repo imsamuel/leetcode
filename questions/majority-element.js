bd02f926f60b7182db4ea0e081a81bdc6ac468c7
samuelyapwoonpin <samuelywp@gmail.com>
2020-08-09 13:26:29 +0800

    fix: Majority Element solution and test in wrong dir

--- questions/majority-element.js
+++ questions/majority-element.js
@@ -1,4 +1,4 @@
-// https://leetcode.com/problems/majority-element/
+// https://leetcode.com/problems/majority-element
 //
 // Given an array of size n, find the majority element. The majority element is the element that
 // appears more than [n/2] times.
